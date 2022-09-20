d75c702dc8ba8fa27a1bd53d1b95e3acc8e235c8
root <root@localhost.localdomain>
2022-09-20 10:07:36 +0900

    pull 적용

--- src/test/java/com/example/made_centos_0916/MadeCentOs0916ApplicationTests.java
+++ src/test/java/com/example/made_centos_0916/MadeCentOs0916ApplicationTests.java
@@ -1,6 +1,5 @@
-package com.example.made_centos_0916;
-
-import org.junit.jupiter.api.Test;
+//package com.example.made_centos_0916
+/*import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,4 +9,4 @@ class MadeCentOs0916ApplicationTests {
     void contextLoads() {
     }
 
-}
+}*/
